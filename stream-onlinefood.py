6c066aefc0f08c643454bdc087bed7bd7533b797
Allun Samudra Harum <allunsamudraa@gmail.com>
2024-07-21 21:21:02 +0700

    Add files via upload

---
+++ stream-onlinefood.py
@@ -0,0 +1,39 @@
+import pickle
+import streamlit as st
+
+# membaca model
+model = pickle.load(open('model.sav', 'rb'))
+
+#judul web
+st.title('Prediksi Feedback')
+
+#membagi kolom
+col1, col2 = st.columns(2)
+
+with col1 :
+    Age = st.text_input ('input Age')
+
+with col2 :
+    Gender = st.text_input ('input Gender')
+
+with col1 :
+    Occupation = st.text_input ('input Occupation')
+
+with col2 :
+    MonthlyIncome = st.text_input ('input MonthlyIncome')
+
+with col1 :
+    EducationalQualifications = st.text_input ('input EducationalQualifications')
+
+# code untuk prediksi
+feedback_pred = ''
+
+# membuat tombol untuk prediksi
+if st.button('Test Prediksi Feedback'):
+    feedback_prediction = model.predict([[Age, Gender, Occupation, MonthlyIncome, EducationalQualifications]])
+
+    if(feedback_prediction(0) == 0):
+        feedback_pred = "Negative"
+    else:
+        feedback_pred = "Positive"
+st.success(feedback_pred)
